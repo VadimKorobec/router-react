5fe20e3053e417625e83c3b2bb4d19d509359126
VadimKorobec <_s_e_t__@ukr.net>
2024-12-11 13:49:04 +0200

    add some code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,49 @@
 // Challenge / Exercise
 
-import { Route, Routes } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 import HomePage from "./pages/Home";
 import EventsPage from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
-import NotFoundPage from './pages/NotFound.jsx'
+import NotFoundPage from "./pages/NotFound.jsx";
 import RootLayout from "./pages/Root.jsx";
 import EventsRootLayout from "./pages/EventsRoot.jsx";
 
+const fetchEvents = async () => {
+  const response = await fetch("http://localhost:8080/events");
 
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+  if (!response.ok) {
+  }
+
+  const resData = await response.json();
+  return resData.events;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "events",
+        element: <EventsRootLayout />,
+        children: [
+          { index: true, element: <EventsPage />, loader: fetchEvents },
+          { path: ":eventId", element: <EventDetailPage /> },
+          { path: "new", element: <NewEventPage /> },
+          { path: ":eventId/edit", element: <EditEventPage /> },
+        ],
+      },
+    ],
+  },
+  { path: "*", element: <NotFoundPage /> },
+]);
 
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<RootLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="events" element={<EventsRootLayout />}>
-            <Route index element={<EventsPage />} />
-            <Route path=":eventId" element={<EventDetailPage />} />
-            <Route path="new" element={<NewEventPage />} />
-            <Route path=":eventId/edit" element={<EditEventPage />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
